16e340e2378c76ebdc84614677f468c8597754d8
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-05-17 14:00:46 +0300

    exe1.09 done (part1.3 done)

---
+++ part1/1.3/app3 (pingpong)/index.js
@@ -0,0 +1,32 @@
+const Koa = require('koa')
+const koaRouter = require('koa-router')
+
+const app = new Koa()
+const router = new koaRouter()
+
+const PORT = process.env.PORT || 3000
+
+let number = 0
+
+
+app.use( async (ctx, next) => {
+  try {
+    await next()
+  } catch(err) {
+    console.log(err.status)
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
+})
+
+router.get('pingpong', '/pingpong', (context) => {
+  context.status = 200
+  context.body = number
+  number = number + 1
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+
+app.listen(PORT)
