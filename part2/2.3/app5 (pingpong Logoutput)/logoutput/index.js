8b50ee38eb61fbcf834acfef2ec4a65c52b460e5
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-05-23 22:37:11 +0300

    exe2.5 and 2.6 done

---
+++ part2/2.3/app5 (pingpong Logoutput)/logoutput/index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+
+
+const PORT = process.env.PORT || 8080
+
+const randomString = Math.random().toString(36).substr(2, 15)
+let string = ""
+
+const getRandomString = () => {
+    let date_ob_String = new Date().toLocaleString('en-US', { timeZone: 'Europe/Helsinki' });
+    let date_ob = new Date(date_ob_String);
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+
+    console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + ": " + randomString);
+    string = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + ": " + randomString
+    
+    setTimeout(getRandomString, 5000);
+
+}
+
+const getPongs = async () => {
+  const res = await axios.get("http://pingpong-svc:2345/pingpong")
+  return res.data
+}
+
+  
+app.get('/', async (req, res) => {
+  console.log("moikkkkaaaa")
+  const pongs = await getPongs() 
+  res.send(`
+    <div>${process.env.MESSAGE}</div>
+    <div>${string}</div>
+    <div>Ping / Pongs: ${pongs}</div>
+  `)
+})
+
+getRandomString();  
+
+app.listen(PORT, "0.0.0.0", () => {console.log(`Server started in port ${PORT}`) })
+  
