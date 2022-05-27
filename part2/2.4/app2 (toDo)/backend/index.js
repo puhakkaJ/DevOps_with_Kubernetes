436772f4cccc78e42a274f414dc1f1f43c9d1d42
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-05-27 21:04:55 +0300

    exe2.8 done

---
+++ part2/2.4/app2 (toDo)/backend/index.js
@@ -0,0 +1,83 @@
+const http = require('http')
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+const fs = require('fs')
+var cors = require('cors')
+const bodyParser = require('body-parser');
+
+
+const PORT = process.env.PORT || 5050
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors())
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'image.jpg')
+
+
+const { Pool, Client } = require("pg");
+console.log(process.env.POSTGRES_PASSWORD)
+const pool = new Pool({
+  user: "kubeuser",
+  host: "postgres-svc",
+  database: "pingpong",
+  password: process.env.POSTGRES_PASSWORD,
+  port: "5432"
+});
+
+const fileAlreadyExists = async () => new Promise(res => {
+  fs.stat(filePath, (err, stats) => {
+    if (err || !stats) return res(false)
+    return res(true)
+  })
+})
+
+const fetchImage = async () => {
+  await new Promise(res => fs.mkdir(directory, (err) => res()))
+  const response = await axios.get('https://picsum.photos/1200', {
+    responseType: 'stream'
+  })
+  response.data.pipe(fs.createWriteStream(filePath))
+
+  setTimeout(fetchImage, 86400000);
+}
+
+fetchImage();
+
+app.get('/image', async (req, res) => {
+  if (!(fileAlreadyExists)) {await fetchImage()}
+  res.sendFile(filePath)
+})
+
+app.get('/todos', async (req, result) => {
+  pool.query(
+    "SELECT text FROM todo",
+    (err, res) => {
+      console.log(err, res);
+      let todos1 = res.rows
+      let todos = []
+      todos1.forEach(todo => {
+        todos.push(todo.text)
+      });
+      result.json(todos)
+    }
+  );
+  
+})
+
+app.post('/todos', async (req, result) => {
+  console.log(req.body.new)
+  pool.query(
+    `INSERT INTO todo VALUES ('${req.body.new}')`,
+    (err, res) => {
+      console.log(err, res);
+      
+      result.sendStatus(200);
+    }
+  );
+})
+
+
+app.listen(PORT, () => {console.log(`Server started in port ${PORT}`) })
