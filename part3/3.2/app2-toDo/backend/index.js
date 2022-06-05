063699bcca042921a0e8869998b9be83ddc27c07
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-06-05 21:21:30 +0300

    test workflow

--- part3/3.2/app2-toDo/backend/index.js
+++ part3/3.2/app2-toDo/backend/index.js
@@ -16,7 +16,7 @@ const filePath = path.join(directory, 'image.jpg')
 
 const { Pool, Client } = require("pg");
 
-console.log("Starting logging todos 52:fiis in77")
+console.log("Starting logginnng todos 52:fiis in77")
 
 const pool = new Pool({
   user: "kubeuser",
