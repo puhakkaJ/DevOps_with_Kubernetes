d7164350970ed8ce056a912c0a00c91c41c7302c
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-07-02 08:42:09 +0300

    exe4.08

---
+++ part4/4.2/app2 (toDo)/backend/index.js
@@ -0,0 +1,109 @@
+const http = require('http')
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+const fs = require('fs')
+var cors = require('cors')
+const bodyParser = require('body-parser');
+const NATS = require('nats')
+
+
+const PORT = process.env.PORT || 5050
+
+const nc = NATS.connect({
+  url: process.env.NATS_URL || 'nats://nats:4222'
+})
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors())
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'image.jpg')
+
+
+const { Pool, Client } = require("pg");
+console.log(process.env.POSTGRES_PASSWORD)
+const pool = new Pool({
+  user: "kubeuser",
+  host: "postgres-svc",
+  database: "pingpong",
+  password: process.env.POSTGRES_PASSWORD,
+  port: "5432"
+});
+
+const fileAlreadyExists = async () => new Promise(res => {
+  fs.stat(filePath, (err, stats) => {
+    if (err || !stats) return res(false)
+    return res(true)
+  })
+})
+
+const fetchImage = async () => {
+  await new Promise(res => fs.mkdir(directory, (err) => res()))
+  const response = await axios.get('https://picsum.photos/1200', {
+    responseType: 'stream'
+  })
+  response.data.pipe(fs.createWriteStream(filePath))
+
+  setTimeout(fetchImage, 86400000);
+}
+
+fetchImage();
+
+app.get('/image', async (req, res) => {
+  if (!(fileAlreadyExists)) {await fetchImage()}
+  res.sendFile(filePath)
+})
+
+app.get('/todos', async (req, result) => {
+  pool.query(
+    "SELECT * FROM todo",
+    (err, res) => {
+      console.log(err, res);
+      let todos1 = res.rows
+      console.log(todos1)
+      let todos = []
+      todos1.forEach(todo => {
+        todos.push(todo)
+      });
+      result.json(todos)
+    }
+  );
+})
+
+app.post('/todos', async (req, result) => {
+  if (req.body.new.length < 141) {
+    pool.query(
+      `INSERT INTO todo (text, done) VALUES ('${req.body.new}', false)`,
+      (err, res) => {
+        console.log(req.body.new);
+        nc.publish('todo_status', JSON.stringify({message: `New TODO '${req.body.new}' added!`}))
+        result.sendStatus(200);
+      }
+    );
+  } else {
+    console.log("Your sent TODO is too long. Limit is 140 characters!")
+  }
+})
+
+app.put('/todos/:id', async (req, result) => {
+  console.log("put", req.params.id)
+  pool.query(
+    `UPDATE todo SET done=true WHERE id = ${req.params.id}`,
+    (err, res) => {
+      console.log(req.body);
+      nc.publish('todo_status', JSON.stringify({message: `TODO with id ${req.params.id} set to DONE`}))
+      result.sendStatus(200);
+    }
+  );
+})
+
+app.get('/healthz', async (req, res) => {
+  const { rows } = await pool.query(`SELECT 1;`)
+  if (rows.length === 1) res.sendStatus(200)
+  else res.sendStatus(500)
+})
+
+
+app.listen(PORT, () => {console.log(`Server started in port ${PORT}`) })
---
+++ part4/4.2/app2 (toDo)/frontend/src/Main.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const url = 'http://localhost:8082/image'
+
+const Main = () => {
+    const [input, setInput] = useState("")
+    const [todos, setTodos] = useState([])
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+      await axios
+        .post('http://localhost:8082/todos', { new: input})
+        .then(() => console.log('Todo Created'))
+        .catch(err => {
+          console.error(err);
+        });
+      
+      setInput("")
+      await axios
+        .get('http://localhost:8082/todos')
+        .then(response => {
+          console.log('promise fulfilled2')
+          setTodos(response.data)
+        })
+    }
+    
+    useEffect(() => {
+      console.log('effect')
+      axios
+        .get('http://localhost:8082/todos')
+        .then(response => {
+          console.log('promise fulfilled')
+          setTodos(response.data)
+        })
+    }, [])
+  
+  
+    const handleChange = (event) => {
+      setInput(event.target.value)
+    }
+
+    const handleDone = async (id) => {
+        const { data } = await axios.put(`http://localhost:8082/todos/${id}`)
+        console.log(id, data)
+        if (data) {
+            await axios
+                .get('http://localhost:8082/todos')
+                .then(response => {
+                    console.log('promise fulfilled2')
+                    setTodos(response.data)
+                })
+        }
+      }
+  
+  
+    return (
+      <div>
+        <img alt="daily" src={url} style={{ width: 700, height: 700}} />
+        <div>
+          <form method="POST" onSubmit={handleSubmit}>
+          <input value={input} onChange={handleChange}/>
+            <button variant="contained" type="submit" >
+              Save
+            </button>
+          </form>
+        </div>
+        <br></br>
+        <ul>
+          {todos.map(todo => 
+            <li key={todo.id}>{todo.text} {todo.done === true ? <span style={{ color: 'green' }}>DONE</span> : <button onClick={() => handleDone(todo.id)} style={{ color: 'red' }} >Mark done</button>}</li> )}
+        </ul>
+      </div>
+    )
+  }
+  
+  export default Main;
