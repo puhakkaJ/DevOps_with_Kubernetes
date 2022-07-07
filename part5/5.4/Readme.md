ccb68f430f667e4525e579524b14c4fe9c680ac4
Jenni Puhakka <jenni.puhakka@aalto.fi>
2022-07-07 13:21:26 +0300

    exe5.4 done

---
+++ part5/5.4/Readme.md
@@ -0,0 +1,24 @@
+## Exercise 5.04: Platform comparison
+
+### Rancher pros
+- Easy to use
+- Cross cloud compatible
+- Very Docker-friendly
+
+### OpenStack pros
+- Private cloud
+- Flexible in use
+- Supported by many top companies
+
+### Rancher cons
+- Hosting Rancher can be complicated
+- Still
+- You will have a certain technology entry limit (Docker images)
+
+### OpenStack cons
+- Installation challenges. Deploying OpenStack isn't as simple
+- Long-term support
+- Documentation
+
+
+## Exercise 5.06: Landscape
---
+++ part5/5.4/app5 (pingpong Logoutput)/pingpong/index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+const { Pool, Client } = require("pg");
+console.log(process.env.POSTGRES_PASSWORD)
+const pool = new Pool({
+  user: "kubeuser",
+  host: "postgres-svc",
+  database: "pingpong",
+  password: process.env.POSTGRES_PASSWORD,
+  port: "5432"
+});
+
+
+app.get('/pingpong',async (req, result) => {
+  console.log("here")
+  pool.query(
+    "SELECT number FROM pp WHERE latest=true",
+    (err, res) => {
+      console.log(err, res);
+      let number = parseInt(res.rows[0].number)
+      number = number + 1
+      let num = `${number}`
+      console.log(number)
+      pool.query(
+        `UPDATE pp SET number=${number} WHERE latest=true`,
+        (err, res) => {
+          console.log(err, res);
+        }
+      );
+      result.send(num)
+    }
+  );
+})
+
+app.get('/healthz', async (req, res) => {
+  const { rows } = await pool.query(`SELECT 1 FROM pp;`)
+  console.log(rows)
+  if (rows.length === 1) res.sendStatus(200)
+  else res.sendStatus(500)
+})
+
+
+app.listen(PORT, "0.0.0.0", () => {console.log(`Server started in port ${PORT}`) })
